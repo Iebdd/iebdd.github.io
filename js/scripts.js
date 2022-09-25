59da8388376ff8e10e0aaa678ff3eac2d162409b
Andreas Hofer <andreas.hofer5@me.com>
2022-09-25 18:13:07 +0200

    It ain't much but it's something

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
     localStorage.setItem('darkMode', 0);
-    var json = JSON.parse('data/data.json')
+    let url = 'js/data.json';
+    var data;
+    fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then((text) => data = text)
+        .catch((error) => console.log(`Could not fetch: ${error}`));
+    console.log("I am doing things!");
     
-})
+});
 
 $(function() {
 
@@ -10,16 +21,14 @@ $(function() {
         $('#btn').show();
     });
     $('#btn').on('click', function() {
-        var select_value = []
+        var select_value = [];
         select_value[0] = $('#Datacenter').val();
         select_value[1] = $('#World').val();
-
-        get_values(select_value)
     });
 });
 $(function() {
     $("#Alphabetical").click(function() {
-
+        console.log("Things happen");
     });
 });
 
@@ -29,13 +38,11 @@ $(function() {
             localStorage.setItem('darkMode', 1);
             $("body").addClass('dark');
             $('#mode-text').html('Dark mode');
-            $("#mode-text").addClass('light');
         }
         else if(localStorage.getItem('darkMode') && localStorage.getItem('darkMode') == 1) {
             localStorage.setItem('darkMode', 0);
             $("body").removeClass('dark');
             $('#mode-text').html('Light mode');
-            $("#mode-text").removeClass('light');
 
         }
     });
