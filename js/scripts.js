0760c5083f593c12c4e8bd14ede9d12242659e04
Andreas Hofer <andreas.hofer5@me.com>
2022-09-30 16:45:20 +0200

    Added option selection functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
 $(document).ready(function(){
-    localStorage.setItem('darkMode', 0);
-    let url = 'js/data.json';
-    var data;
-    fetch(url)
+    localStorage.setItem('darkMode', 0); 
+});
+
+/*function get_data(url) {
+
+    var data = fetch(url)
         .then((response) => {
             if (!response.ok) {
                 throw new Error(`HTTP error: ${response.status}`);
             }
-            return response.text();
+            response.json();
         })
-        .then((text) => data = text)
+        .then((success) => {    return success.data;
+        })
         .catch((error) => console.log(`Could not fetch: ${error}`));
-    console.log("I am doing things!");
-    
-});
+    return data;
+};*/
 
-$(function() {
+function adjust_listing(string) {
 
     $('#Datacenter').on('change', function() {
         $('#btn').show();
@@ -25,10 +27,67 @@ $(function() {
         select_value[0] = $('#Datacenter').val();
         select_value[1] = $('#World').val();
     });
-});
+};
+
 $(function() {
     $("#Alphabetical").click(function() {
-        console.log("Things happen");
+        $.ajax('js/data.json', 
+        {
+            dataType: 'json',
+            timeout: 500,
+            success: function (data, status, xhr) {
+                var string = ['<option value=\'', '\'>', '</option>'];
+                var filtered = data["data"]["spells"].filter(function (el) {
+                    return el != '';
+                });
+                filtered.sort((a, b) => a.localeCompare(b));
+                var list = '';
+                for (var i = 0; i < filtered.length; i++) {
+                    list += string[0] + filtered[i] + string[1] + filtered[i] + string[2];
+                }
+                $("#Spells").html(list);
+
+            },
+            error: function (jqXhr, textStatus, errorMessage) {
+                $('#Sorting').append('Error: ' + errorMessage);
+            }
+        })
+    });
+    $("#Spell_Level").click(function() {
+        $.ajax('js/data.json', 
+        {
+            dataType: 'json',
+            timeout: 500,
+            success: function (data, status, xhr) {
+                var string = ['<option value=\'', '\'>', '</option>', '\' disabled=\'disabled',' Level:'];
+                var ordinal = ['<sup>st</sup>', '<sup>nd</sup>', '<sup>rd</sup>', '<sup>th</sup>']
+                var list = '';
+                var spells = data["data"]["spells"];
+                var length = spells.length;
+                var level = 1;
+                list += string[0] + 'Cantrips:' + string[3] + string[1] + 'Cantrips:' + string[2];
+                for (var i = 0; i < length; i++) {
+                    if (spells[i] === '') {
+                        if (level > 3) {
+                            list += string[0] + level + ordinal[3] + string[4] + string[3] + string[1] + level + ordinal[3] + string[4] + string[2];
+                            level++;
+                            continue;
+                        }
+                        else {
+                            list += string[0] + level + ordinal[level - 1] + string[4] + string[3] + string[1] + level + ordinal[level - 1] + string[4] + string[2];
+                            level++;
+                            continue;
+                        }
+                    }
+                    list += string[0] + spells[i] + string[1] + spells[i] + string[2];
+                }
+                $("#Spells").html(list);
+            },
+
+            error: function (jqXhr, textStatus, errorMessage) {
+                $('#Sorting').append('Error: ' + errorMessage);
+            }
+        });
     });
 });
 
