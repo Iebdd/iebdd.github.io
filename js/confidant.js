bef1739b050c7534e7ad3707e5a3152bc3c94dbb
Andreas Hofer <andreas.hofer5@me.com>
2022-11-27 01:40:07 +0100

    Furthered the menu

---
+++ js/confidant.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 167px and the left margin of the page content to 167px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "167px";
+  document.getElementById("main").style.marginLeft = "167px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+} 
